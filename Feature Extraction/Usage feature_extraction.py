9268613607910c2d893408bfc56c097e04ad8b14
mcannistra <95096730+Cannistra94@users.noreply.github.com>
2024-05-29 10:21:54 -0400

    Create Usage feature_extraction.py

---
+++ Feature Extraction/Usage feature_extraction.py
@@ -0,0 +1,5 @@
+python feature_extraction.py <t2_flag>
+
+<t2_flag>: indicate whether T2 sequence is available or not. Must be binary (1 if available, 0 otherwise)
+
+The script will extract radiomics features from pre-processed images
